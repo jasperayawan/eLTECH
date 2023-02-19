035b8094a6e95e9e5deace04c3964780e9384d4b
jasperayawan <ejayawan22@gmail.com>
2023-02-19 17:07:10 +0800

    updated login modal

--- main.js
+++ main.js
@@ -84,5 +84,54 @@ const addBtn = document.querySelectorAll('.addToCart').forEach(e => {
 })
 
 
-// form validation
+// Practice Javascript
 
+//Creating and inserting elements
+// const header = document.querySelector('header')
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+
+// message.innerHTML = 'We use cookied for improved functionality and anylytics. <button class="btn btn--close-cookie" onclick="hbutton()">Got it!</button>';
+// header.prepend(message);
+// header.append(message.cloneNode(true));
+// header.append(message)
+// function hbutton(){
+//     alert("hello!")
+// }
+
+    // Delete Elements
+// document.querySelector('.btn--close-cookie').addEventListener('click', function(){
+//     message.parentElement.removeChild(message);
+// })
+
+
+const header = document.querySelector('header');
+const popUpMessage = document.createElement('div');
+popUpMessage.classList.add('pop-up-message');
+popUpMessage.innerHTML = `<div class="div-child"><form class="form-control js_form">
+    <h3>Login</h3>
+    <input type="text" name="fname" class="form-control" id="name" placeholder="Enter Email" required>
+    <input type="email" name="email" class="form-control" id="email" placeholder="Enter Password" required>
+    <p class="js-p">or</p>
+    <div class="buttons">
+        <button class="facebook">facebook</button>
+        <button class="gmail">Gmail</button>
+        <button class="instagram">Instagram</button>
+    </div>
+    <div class="btn-loginn">
+        <button class="btn btn-primary">Login</button>
+    <div>
+    <button class="btn--close-cookie btn-close"></button>
+    </form>
+    </div>
+    `
+
+
+popUpMessage.style.display = "flex";
+popUpMessage.style.justifyContent = "center";
+popUpMessage.style.alignItems = "center";
+header.append(popUpMessage);
+
+document.querySelector('.btn--close-cookie').addEventListener('click', function(){
+    popUpMessage.parentElement.removeChild(popUpMessage)
+})
