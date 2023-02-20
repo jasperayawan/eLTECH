41456c3aae23c2af05b3e8d9a1f73ec5c681ee3c
jasperayawan <ejayawan22@gmail.com>
2023-02-20 22:31:06 +0800

    update cart

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-const header = document.querySelector('header');
+const header = document.querySelector('.header');
 const popUpMessage = document.createElement('div');
 popUpMessage.classList.add('pop-up-message');
-popUpMessage.innerHTML = `<div class="div-child"><form class="form-control js_form">
+popUpMessage.innerHTML = `<div class="div-child"><form class="js_form">
     <h3>Login</h3>
-    <input type="text" name="fname" class="form-control" id="name" placeholder="Enter Email" required>
-    <input type="email" name="email" class="form-control" id="email" placeholder="Enter Password" required>
+    <div class="formOneJs">
+        <input type="text" name="fname" id="name" placeholder="Enter Email" required>
+    </div>
+    <div class="formTwoJs">
+    <input type="email" name="email" id="email" placeholder="Enter Password" required>
+    </div>
     <p class="js-p">or</p>
     <div class="buttons">
         <button class="facebook">facebook</button>
@@ -26,5 +30,59 @@ popUpMessage.style.alignItems = "center";
 header.append(popUpMessage);
 
 document.querySelector('.btn--close-cookie').addEventListener('click', function(){
-    popUpMessage.parentElement.removeChild(popUpMessage)
+    // popUpMessage.parentElement.removeChild(popUpMessage)
+    popUpMessage.remove();
 })
+
+
+
+
+// add to cart 
+
+// const product = [
+//     {
+//         id: 0,
+//         image: 'assets/python.jpg',
+//         title: 'Python for beginners',
+//         price: 120,
+//     },
+//     {
+//         id: 1,
+//         image: 'assets/javascript-meta-1652702081069.jpg',
+//         title: 'Javascript algorithm',
+//         price: 120,
+//     },
+//     {
+//         id: 2,
+//         image: 'assets/mern.png',
+//         title: 'Learn MERN stack',
+//         price: 120,
+//     },
+//     {
+//         id: 3,
+//         image: 'assets/unreal-blog2.png',
+//         title: 'Unreal game development',
+//         price: 120,
+//     }
+// ];
+
+// const categories  = [...new Set(product.map((item) => {
+//     return item
+// }))]
+
+//     let i = 0;
+//     document.getElementById('root').innerHTML = categories.map((item) => {
+//         var {image, title, price} = item; 
+//         return(
+//             `div class='box'>
+//                 <div class='img-box'>
+//                     <img class='images' src=${image}>
+//                 </div>
+//                 <div class='bottom'>
+//                 <p>${title}<p>
+//                 <h2>php ${price}.00</h2>`+
+//                 "<button class='buttonCart' onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+//                 `</div>
+//                 </div>`
+//         )
+//     }).join('') 
